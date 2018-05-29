9539b854624418cc5972bd38ecb77bc04f7088e2
Doug Beney <contact@dougie.io>
2018-05-29 08:53:28 -0400

    playin around

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <gtk/gtk.h>
 
+void print_hello(GtkApplication *app, gpointer user_data) {
+}
+
 int main(int argc, char **argv) {
 	GtkBuilder *builder;
-	GObject *window;
-	GError *error = NULL;
 
 	gtk_init(&argc, &argv);
 
 	builder = gtk_builder_new_from_file("builder.ui");
 
-	window = gtk_builder_get_object(builder, "window");
+	GObject *window = gtk_builder_get_object(builder, "window");
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	
+	GObject *quit_btn = gtk_builder_get_object(builder, "quit-button");
+	g_signal_connect(quit_btn, "clicked", G_CALLBACK(print_hello), NULL);
+	
 
 	gtk_main();
 	return 0;
